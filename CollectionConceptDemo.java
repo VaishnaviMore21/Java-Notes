d323b4d53404e8f9dd5e0cc325a449baeae0b08a
Vaishnavi More <143414333+VaishnaviMore21@users.noreply.github.com>
2025-09-24 19:41:01 +0530

    Create  CollectionConceptDemo.java

---
+++ CollectionConceptDemo.java
@@ -0,0 +1,11 @@
+public class CollectionConceptDemo {
+    public static void main(String[] args) {
+        // Collection of integers (conceptual)
+        int[] numbers = {10, 20, 30, 40, 50};
+        
+        System.out.println("Collection of numbers:");
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
